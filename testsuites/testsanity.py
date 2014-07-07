b47d1cf5fadb6f2315caef2363bca4838385da0e
ksharma <ksharma@suse.com>
2014-07-07 17:29:04 +0200

    line indentations

--- testsuites/testsanity.py
+++ testsuites/testsanity.py
@@ -21,62 +21,66 @@ class TestSanity(basetest.Basetest):
                                 cls.ctx['workingdir'])
         
     def test00_createDirs(self):
-        log.info('starting test0_createDirs')
+        log.info('++++++++++starting test0_createDirs++++++++++++++')
         if not self.ctx.has_key('workingdir'):
             self.ctx['workingdir'] = '~/cephdeploy-cluster'
         general.createDir(self.ctx['workingdir'])
-        for node in self.ctx['allnodes']:
-            general.createDir('/var/lib/ceph/osd',node)
-            general.createDir('/var/lib/ceph/bootstrap-osd',node)
-        log.info('Completed test0_createDirs')
+        #for node in self.ctx['allnodes']:
+            #general.createDir('/var/lib/ceph/osd',node)
+            #general.createDir('/var/lib/ceph/bootstrap-osd',node)
+        log.info('++++++++++Completed test0_createDirs+++++++++++++')
       
     def test01_AddRepo(self):
-        log.info('starting test1_AddRepo')
+        log.info('++++++++++starting test1_AddRepo+++++++++++++++++')
         url = self.config.get('env','repo_baseurl')
         zypperutils.addRepo('ceph', url)
-        log.info('Completed test1_AddRepo')
+        log.info('++++++++++Completed test1_AddRepo++++++++++++++++')
     
     def test02_InstallCephDeploy(self):
-        log.info('starting the test test2_InstallCephDeploy')
+        log.info('++++++++++starting the test test2_InstallCephDeploy\
+                  +++++++++++++++')
         zypperutils.installPkg('ceph-deploy')
-        log.info('Completed test2_InstallCephDeploy')
+        log.info('+++++++++++++++Completed test2_InstallCephDeploy\
+                  +++++++++++++++')
     
     def test03_DeclareInitialMons(self):
-        log.info('starting test3_DeclareInitialMons')
+        log.info('+++++++++++++++++starting test3_DeclareInitialMons \
+                 ++++++++++++++++++')
         cephdeploy.decalreInitialMons(self.ctx['initmons'], 
                                       self.ctx['workingdir'])
-        log.info('Completed test3_DeclareInitialMons')
+        log.info('+++++++++++++++++Completed test3_DeclareInitialMons \
+                  +++++++++++++++++')
     
     def test04_InstallCeph(self):
-        log.info('starting test4_installCeph')
+        log.info('++++++++++++starting test4_installCeph+++++++++++++')
         cephdeploy.installNodes(self.ctx['allnodes'], 
                                 self.ctx['workingdir'])
-        log.info('Completed test4_installCeph')
+        log.info('++++++++++++Completed test4_installCeph++++++++++++')
         
     def test05_CreateInitialMons(self):
-        log.info('starting test5_createInitialMons')
+        log.info('+++++++++starting test5_createInitialMons+++++++++')
         cephdeploy.createInitialMons(self.ctx['initmons'], 
                                      self.ctx['workingdir'])
-        log.info('Completed test5_createInitialMons')
+        log.info('+++++++++Completed test5_createInitialMons++++++++')
     
     def test06_PrepareActivateOSDs(self):
-        log.info('starting test6_PrepareActivateOSDs')
+        log.info('+++++++++starting test6_PrepareActivateOSDs++++++++')
         cephdeploy.PrepareActivateOSDs(self.ctx['osds'], 
                                        self.ctx['workingdir'])
-        log.info('Completed test6_PrepareActivateOSDs')
+        log.info('+++++++++Completed test6_PrepareActivateOSDs+++++++')
     
     def test07_AdminNodes(self):
-        log.info('starting test7_AdminNodes')
+        log.info('++++++++++starting test7_AdminNodes++++++++++++++++')
         cephdeploy.addAdminNodes(self.ctx['allnodes'], 
                                  self.ctx['workingdir'])
-        log.info('completed test7_AdminNodes')
+        log.info('++++++++++completed test7_AdminNodes+++++++++++++++')
     
     def test08_ValidateCephStatus(self):
-        log.info('starting test8_ValidateCephStatus')
+        log.info('++++++++++starting test8_ValidateCephStatus+++++++')
         fsid = monitoring.getFSID(self.ctx['workingdir'])
         status = monitoring.getCephStatus()
         if fsid not in status:
-            raise Exception, "fsid %s was not found in ceph status %s" \
+            raise Exception, "fsid %s was not found in ceph status %s"\
                               % (fsid,status)
         active_clean = False
         counter = 0
@@ -98,29 +102,35 @@ class TestSanity(basetest.Basetest):
                          ceph status')
         if 'health HEALTH_OK' in status:
             log.warning('cluster health is OK and PGs are active+clean') 
-        log.info('completed test8_ValidateCephStatus')
+        log.info('+++++++++completed test8_ValidateCephStatus++++++++')
     
     def test09_ValidateCephDeployVersion(self):
-        log.info('starting test9_ValidateCephVersion')
-        expVersion = cephdeploy.getExpectedVersion(self.config.get('env','repo_baseurl'))
+        log.info('+++++++++starting test9_ValidateCephVersion++++++++')
+        expVersion = cephdeploy.getExpectedVersion(
+                                self.config.get('env','repo_baseurl'))
         actVersion = cephdeploy.getActuaVersion()
         if actVersion not in expVersion:
-            raise Exception, "expected '%s' and actual '%s' versions did not match" % (expVersion,actVersion)
-        log.info('completed test9_ValidateCephVersion')
+            raise Exception, "expected '%s' and actual '%s' versions \
+                              did not match" % (expVersion,actVersion)
+        log.info('++++++++completed test9_ValidateCephVersion++++++++')
      
     def test10_ValidateCephVersion(self):
-        log.info('starting test10_ValidateCephVersion')
-        expVersion = monitoring.getExpectedVersion(self.config.get('env','repo_baseurl'))
+        log.info('++++++++++++++++starting test10_ValidateCephVersion\
+                  ++++++++++++++++')
+        expVersion = monitoring.getExpectedVersion(
+                     self.config.get('env','repo_baseurl'))
         actVersion = monitoring.getActuaVersion()
         if actVersion not in expVersion:
-            raise Exception, "expected '%s' and actual '%s' versions did not match" % (expVersion,actVersion)
-        log.info('completed test10_ValidateCephVersion')
+            raise Exception, "expected '%s' and actual '%s' \
+                versions did not match" % (expVersion,actVersion)
+        log.info('++++++++++++++++completed test10_ValidateCephVersion\
+                  ++++++++++++++++')
      
      
     @classmethod
     def teardown_class(self):
-        log.info('starting teardown_class')
+        log.info('++++++++++++++starting teardown_class+++++++++++++')
         cephdeploy.cleanupNodes(self.ctx['allnodes'], 
                                 self.config.get('env','repo_name'), 
                                 self.ctx['workingdir'])
-        log.info('Completed teardown_class')
+        log.info('++++++++++++++Completed teardown_class++++++++++++')
