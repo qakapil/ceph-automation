f3f6eb58ed92e13c77deff1a0c66abca4f77ca52
ksharma <ksharma@suse.com>
2014-08-21 12:20:43 +0200

    added testbed module

---
+++ utils/testbed.py
@@ -0,0 +1,65 @@
+from utils import basetest
+from utils import zypperutils
+from utils import cephdeploy
+from utils import monitoring
+from utils import operations
+import logging,time,re, os
+
+log = logging.getLogger(__name__)
+
+def validateCephStatus(self):
+        fsid = monitoring.getFSID()
+        status = monitoring.getCephStatus()
+        if fsid not in status:
+            raise Exception, "fsid %s was not found in ceph status %s"\
+                              % (fsid,status)
+        active_clean = False
+        counter = 0
+        #default_pgs = str(self.ctx['default_pgs']).strip()
+        default_pgs = monitoring.getTotalPGs()
+        while not active_clean:
+            if default_pgs +' active+clean' in status:
+                log.info('placement groups in ceph status were \
+                          active+clean')
+                active_clean = True
+                continue
+            if (counter > 20):
+                raise Exception, 'PGs did not reach active+clean state \
+                                   after 5 mins'
+            log.debug('waiting for 5 seconds for ceph status to update')
+            time.sleep(5)
+            counter += 1
+            status = monitoring.getCephStatus()
+        if 'health HEALTH_WARN clock skew detected' in status:
+            log.warning('health HEALTH_WARN clock skew detected in\
+                         ceph status')
+        if 'health HEALTH_OK' in status:
+            log.warning('cluster health is OK and PGs are active+clean')
+
+
+
+def validateMonStat(self):
+        mon_stat = monitoring.getMonStat()
+        log.info("the mon stat is "+ str(mon_stat))
+        matchObj = re.match( r'.*:(.*) mons at .* quorum (.*?) (.*)', mon_stat, re.M|re.I)
+        assert(len(self.ctx['initmons']) == int(matchObj.group(1))),\
+        "the number of mons active were not as expected"
+        assert(len(self.ctx['initmons']) == len(matchObj.group(2).split(','))),\
+        "the number of mons in quorum were not as expected"
+        assert(sorted(self.ctx['initmons']) == sorted(matchObj.group(3).split(','))),\
+        "the monlist in quorum was not as expected"
+
+
+
+
+def validateDefaultOSDtree(self):
+        str_osd_tree = monitoring.getOSDtree()
+        osd_tree = str_osd_tree.split('\n')
+        for i in range(len(osd_tree)-1):
+            osd_tree[i] = osd_tree[i].split('\t')
+        indx = osd_tree[0].index('weight')
+        for i in range(len(osd_tree)-1):
+            value = osd_tree[i][indx].strip()
+            assert('0' != value),"the weight of the\
+            osd was zero \n"+str_osd_tree
+    
