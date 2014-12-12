fca1a3cef67003505db54415d8ed78af70d20390
ksharma <ksharma@suse.com>
2014-12-12 17:26:29 +0100

    changed exception handling for repo not present

--- utils/cephdeploy.py
+++ utils/cephdeploy.py
@@ -148,7 +148,7 @@ def cleanupNodes(listNodes, reponame):
         try:
             zypperutils.removeAllPkgsFromRepo(reponame, node)
         except Exception as e:
-            log.warning("Error while removing packages ", e)
+            log.warning("Error while removing packages ", str(e))
         cmd = "ssh %s sudo zypper removerepo %s" % (node, reponame)
         rc,stdout,stderr = launch(cmd=cmd)
         if rc != 0:
