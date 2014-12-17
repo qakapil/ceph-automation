685e0e43e9668691a589633687ce703a21f18658
ksharma <ksharma@suse.com>
2014-12-17 13:22:48 +0100

    cloud ISO build029 test changes reverted

--- utils/general.py
+++ utils/general.py
@@ -104,7 +104,7 @@ def mountISO(build_num, staging=True):
     else:
         cmd = 'ssh %s sudo mount -o loop /mounts/dist/ibs/Devel:/Storage:/1.0:/Staging/images/iso/SUSE-Storage-1.0-DVD-x86_64-%s-Media1.iso %s' % (os.environ["CLIENTNODE"], build_num, mount_dir)
     
-    cmd = 'ssh %s sudo mount -o loop /mounts/dist/ibs/SUSE:/SLE-12:/Update:/Products:/Cloud5/images/iso/SUSE-Storage-1.0-DVD-x86_64-%s-Media1.iso %s' % (os.environ["CLIENTNODE"], build_num, mount_dir)
+    #cmd = 'ssh %s sudo mount -o loop /mounts/dist/ibs/SUSE:/SLE-12:/Update:/Products:/Cloud5/images/iso/SUSE-Storage-1.0-DVD-x86_64-%s-Media1.iso %s' % (os.environ["CLIENTNODE"], build_num, mount_dir)
     rc,stdout,stderr = launch(cmd=cmd)
 
 
