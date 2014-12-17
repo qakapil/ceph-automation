feeb547d27a63c600fc9a3271f40bf4a1ce29462
ksharma <ksharma@suse.com>
2014-12-17 10:45:05 +0100

    run ISO tests against cloud image

--- utils/general.py
+++ utils/general.py
@@ -58,7 +58,9 @@ def getISOBuildNum(url):
     if rc != 0:
         raise Exception, "Error while executing the command '%s'. \
                           Error message: '%s'" % (cmd, stderr)
-    build_version = stdout.strip().split('\n')[0]
+    builds = stdout.strip().split('\n')
+    build_version = builds[len(builds)-1]
+    #build_version = stdout.strip().split('\n')[0]
     log.info('ISO build version is - Build'+build_version)
     return 'Build'+build_version
 
