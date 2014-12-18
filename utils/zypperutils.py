84c3f2773280faa36c232480bf956059f45ee7d5
ksharma <ksharma@suse.com>
2014-12-18 13:02:00 +0100

    fixed the command to remove all pkgs from a repo

--- utils/zypperutils.py
+++ utils/zypperutils.py
@@ -99,8 +99,8 @@ def removePkg_expectNotFound(pkgName, node):
         raise Exception, "Error while executing the command '%s'. Error message: '%s'" % (cmd, stderr)
 
 def removeAllPkgsFromRepo(repoName, node):
-    cmd = "ssh %s sudo zypper --non-interactive --no-gpg-checks rm $(zypper --disable-system-resolvables \
-    -s 0 packages -r %s | tail -n +4 | cut -d'|' -f3 | sort -u)"  % (node, repoName)
+    cmd = "ssh %s sudo zypper --non-interactive --no-gpg-checks rm $(ssh %s zypper --disable-system-resolvables \
+    -s 0 packages -r %s | tail -n +4 | cut -d'|' -f3 | sort -u)"  % (node, node, repoName)
     rc,stdout,stderr = launch(cmd=cmd)
     if rc != (0):
         raise Exception, "Error while executing the command '%s'. Error message: '%s'" % (cmd, stdout)
