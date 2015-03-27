6b13bd9e824f181288ddd73d0f00aa0ce7d0a2a7
Joshua Schmid <jschmid@suse.com>
2015-03-27 11:33:01 +0100

    add expected_size to validate_size of image

--- utils/rbd_operations.py
+++ utils/rbd_operations.py
@@ -49,7 +49,8 @@ def validate_image_size(dictImage, size=None):
     assert (rc == 0), "Error while executing the command %s.\
     Error message: %s" % (cmd, stderr)
     act_size_in_mb = (general.convStringToJson(stdout)['size']/1024/1024)
-    assert (str(size) in str(act_size_in_mb)), "Error. Size for image %s was %s MB" % (name, act_size_in_mb)
+    assert (str(size) in str(act_size_in_mb)), "Error. Size for image %s was %s MB - expected it to be %s" \
+                                               % (name, act_size_in_mb, size)
     log.info("validated image - %s in pool %s " % (name, pool))
 
 def validate_image_presence(dictImage, expected_presence=True):
