5d65ecf17a58e1cd59995b2dc3bb385761c900b2
ksharma <ksharma@suse.com>
2015-04-01 01:16:42 +0200

    moving qemu install to rbd suite

--- testsuites/test_rbd.py
+++ testsuites/test_rbd.py
@@ -36,6 +36,7 @@ def setup_module():
         operations.createCephCluster(yaml_data, cfg_data)
     status = monitoring.isClusterReady(300)
     assert status is True, "Ceph cluster was not ready. Failing the test suite"
+    ceph_internal_url = cfg_data.get('env', 'ceph_internal_url')
     general.downloadISOAddRepo(ceph_internal_url, 'Media', 'ceph-internal',
                                os.environ["CLIENTNODE"], iso_name=None, iso_internal=True)
     for pkg in ['rbd-kmp-default','qemu-block-rbd','qemu-tools']:
