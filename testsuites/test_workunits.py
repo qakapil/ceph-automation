63b774317b72ef1b635eaeebaee4790e6a4dd4f7
ksharma <ksharma@suse.com>
2015-04-20 17:56:38 +0200

    added test_workunits

---
+++ testsuites/test_workunits.py
@@ -0,0 +1,78 @@
+from utils import monitoring
+from utils import operations
+from utils import baseconfig
+from utils import general
+
+import inspect
+from ConfigParser import SafeConfigParser
+import logging
+import os
+import sys
+
+log = logging.getLogger(__name__)
+
+cfg_data = None
+yaml_data = {}
+vErrors = []
+
+
+def setup_module():
+    global cfg_data
+    global yaml_data
+    filename = os.environ.get("CFG_FILE", "setup.cfg")
+    cfg_data = SafeConfigParser()
+    cfg_data.read(filename)
+
+    yamlfile = os.environ.get("YAMLDATA_FILE")
+    if yamlfile == None:
+        yamlfile = __name__.split('.')[len(__name__.split('.'))-1]
+        yamlfile = 'yamldata/%s.yaml' % (yamlfile)
+    yaml_data = baseconfig.fetchTestYamlData(yamlfile)
+
+
+    baseconfig.setLogger('cephauto.log', cfg_data)
+    os.environ["CLIENTNODE"] = yaml_data['clientnode'][0]
+    if not monitoring.isClusterReady(60):
+        operations.createCephCluster(yaml_data, cfg_data)
+    status = monitoring.isClusterReady(300)
+    assert status is True, "Ceph cluster was not ready. Failing the test suite"
+
+
+def test_workunit():
+    global vErrors
+    try:
+        yaml_data['test_dir'] = '%s/%s' % (yaml_data['test_dir'], yaml_data['workunit_dir'])
+        url = 'https://github.com/SUSE/ceph/archive/%s.tar.gz' % (yaml_data['ceph_branch'])
+        cmd = 'ssh %s wget -O /tmp/%s.tar.gz %s' % (os.environ["CLIENTNODE"], yaml_data['ceph_branch'], url)
+        general.eval_returns(cmd)
+        cmd = 'ssh %s mkdir -p %s' % (os.environ["CLIENTNODE"], yaml_data['test_dir'])
+        general.eval_returns(cmd)
+        cmd = 'ssh %s tar --strip-components=4 -C %s -xvf /tmp/%s.tar.gz ceph-%s/qa/workunits/%s' % \
+        (os.environ["CLIENTNODE"], yaml_data['test_dir'], yaml_data['ceph_branch'], yaml_data['ceph_branch'], yaml_data['workunit_dir'])
+        general.eval_returns(cmd)
+        test_scripts = os.listdir(yaml_data['test_dir'])
+        for script in test_scripts:
+            run_script(yaml_data['workunit_dir'], script)
+
+    except Exception:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        log.error(str(exc_type)+" : "+str(exc_value)+" : "+str(exc_traceback))
+        sError = str(sys.exc_info()[0])+" : "+str(sys.exc_info()[1])
+        log.error(inspect.stack()[0][3] + "Failed with error - "+sError)
+        vErrors.append(sError)
+        raise Exception(str(sys.exc_info()[1]))
+
+
+def run_script(workunit_dir, script_name):
+    log.info('Executing %s tests from workunit dir') % workunit_dir
+    cmd = 'ssh %s %s/%s' % (os.environ["CLIENTNODE"], yaml_data['test_dir'], script_name)
+    general.eval_returns(cmd)
+
+
+def teardown_module():
+    log.info('++++++completed rbd test suite ++++++')
+    if vErrors:
+        log.info('test suite failed with these errors - '+str(vErrors))
+    else:
+        log.info('starting teardown in teardown_module')
+        general.perNodeCleanUp(yaml_data['allnodes'], 'ceph')
