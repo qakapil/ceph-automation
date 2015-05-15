cec5f6d7f7e5929e6673b15be6e753cdce3933b8
ksharma <ksharma@suse.com>
2015-05-15 04:45:36 +0200

    can not delete all ISOs

--- utils/general.py
+++ utils/general.py
@@ -428,8 +428,8 @@ def downloadISOAddRepo(url, media, reponame, node, iso_name=None, iso_internal=F
     iso_path = '/tmp/%s' % (iso_name)
     log.info('ISO path  is - '+iso_path)
 
-    cmd = 'ssh %s rm /tmp/*iso' %(node)
-    rc,stdout,stderr = launch(cmd=cmd)
+    #cmd = 'ssh %s rm /tmp/*iso' %(node)
+    #rc,stdout,stderr = launch(cmd=cmd)
 
     cmd = 'ssh %s ls %s' %(node, iso_path)
     rc,stdout,stderr = launch(cmd=cmd)
