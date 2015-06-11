fa7e973347c21b111b36324b8993bc96c092054f
ksharma <ksharma@suse.com>
2015-06-11 12:01:01 +0200

    added iscsi tests and suite

---
+++ testsuites/testISCSI.py
@@ -0,0 +1,134 @@
+from utils import basetest
+from utils import zypperutils
+from utils import cephdeploy
+from utils import monitoring
+from utils import general
+from utils import iscsi
+from utils import rbd_operations
+import socket
+import logging, time, os
+from nose.exc import SkipTest
+
+log = logging.getLogger(__name__)
+
+class TestSanity(basetest.Basetest):
+
+    @classmethod
+    def setup_class(cls):
+        filename = os.environ.get("CFG_FILE", "setup.cfg")    
+        cls.fetchIniData(cls, filename)
+        yamlfile = os.environ.get("YAMLDATA_FILE")
+        if yamlfile is None:
+            yamlfile = __name__.split('.')[len(__name__.split('.'))-1]
+            yamlfile = 'yamldata/%s.yaml' % yamlfile
+        cls.fetchTestYamlData(cls,yamlfile)
+        cls.setLogger(cls,'cephauto.log')
+        os.environ["CLIENTNODE"] = cls.ctx['clientnode'][0]
+        monitoring.printRPMVersions(cls.config.get('env','repo_baseurl'))
+
+        general.removeOldRepos(cls.ctx['allnodes'], ['ceph-debug', 'ceph_extras'])
+
+        url = cls.config.get('env','repo_baseurl')
+        for node in cls.ctx['allnodes']:
+            zypperutils.addRepo('ceph', url, node)
+
+        before_cleanup = os.environ.get("BEFORE_CLEANUP")
+        if before_cleanup is not None:
+            log.info('starting teardown for before_cleanup')
+            general.perNodeCleanUp(cls.ctx['allnodes'], 'ceph')
+
+        zypperutils.installPkg('ceph-deploy', os.environ["CLIENTNODE"])
+
+        cephdeploy.declareInitialMons(cls.ctx['initmons'])
+
+        cephdeploy.installNodes(cls.ctx['allnodes'])
+
+        cephdeploy.createInitialMons(cls.ctx['initmons'])
+
+        cephdeploy.osdZap(cls.ctx['osd_zap'])
+
+        cephdeploy.osdPrepare(cls.ctx['osd_prepare'])
+
+        cephdeploy.addAdminNodes(cls.ctx['clientnode'])
+
+        cls.validateCephStatus()
+
+        for image in cls.ctx['images']:
+            rbd_operations.createRBDImage(image)
+
+
+    def setUp(self):
+        if os.environ.get("CLUSTER_FAILED") == "Yes":
+           raise SkipTest("ceph cluster was not active+clean") 
+        log.info('++++++starting %s ++++++' % self._testMethodName)
+
+    def validateCephStatus(self):
+        time.sleep(10)
+        fsid = monitoring.getFSID()
+        status = monitoring.getCephStatus()
+        if fsid not in status:
+            raise Exception, "fsid %s was not found in ceph status %s"\
+                              % (fsid,status)
+        active_clean = False
+        counter = 0
+        default_pgs = monitoring.getTotalPGs()
+        while not active_clean:
+            if default_pgs +' active+clean' in status:
+                log.info('placement groups in ceph status were \
+                          active+clean')
+                active_clean = True
+                continue
+            if (counter > 20):
+                os.environ["CLUSTER_FAILED"] = "Yes"
+                raise Exception, 'PGs did not reach active+clean state \
+                                   after 5 mins'
+            log.debug('waiting for 5 seconds for ceph status to update')
+            time.sleep(5)
+            counter += 1
+            status = monitoring.getCephStatus()
+        if 'health HEALTH_WARN clock skew detected' in status:
+            log.warning('health HEALTH_WARN clock skew detected in\
+                         ceph status')
+        if 'health HEALTH_OK' in status:
+            log.warning('cluster health is OK and PGs are active+clean')
+ 
+
+    def test_ISCSI(self):
+        for iscsi_target in self.ctx['iscsi_targets']:
+            iscsi.targetService(iscsi_target['node'], 'start')
+            iscsi.addBlock(iscsi_target['node'], iscsi_target['block_name'], iscsi_target['rbd_mapped_disk'])
+            iscsi.addIQNTPG(iscsi_target['node'], iscsi_target['iqn'], iscsi_target['tpg'])
+            ip = socket.gethostbyname(iscsi_target['node'])
+            iscsi.addNP(iscsi_target['node'], iscsi_target['iqn'], iscsi_target['tpg'], ip, '3260')
+            iscsi.disableAuth(iscsi_target['node'], iscsi_target['iqn'], iscsi_target['tpg'])
+            iscsi.addLun(iscsi_target['node'], iscsi_target['iqn'], iscsi_target['tpg'], '0', 'MiddlingThingPort', iscsi_target['block_name'])
+            iscsi.enableDemoMode(iscsi_target['node'], iscsi_target['iqn'], iscsi_target['tpg'])
+            iscsi.enableTPG(iscsi_target['node'], iscsi_target['iqn'], iscsi_target['tpg'])
+            iscsi.demoModeWriteProtect(iscsi_target['node'], iscsi_target['iqn'], iscsi_target['tpg'])
+
+            block = iscsi.discoverLoginTarget(iscsi_target['client_node'], iscsi_target['node'], iscsi_target['iqn'], iscsi_target['tpg'], '3260')
+            drive = iscsi.partitionIBlock(iscsi_target['client_node'], block)
+            iscsi.createFSMount(iscsi_target['client_node'], drive, 'xfs')
+
+
+
+
+
+
+
+    def tearDown(self):
+        log.info('++++++completed %s ++++++' % self._testMethodName)
+
+
+        
+         
+    @classmethod
+    def teardown_class(self):
+        after_cleanup = os.environ.get("AFTER_CLEANUP")
+        if after_cleanup == None:
+            log.info('skipping teardown for after_cleanup')
+            return
+        log.info('++++++++++++++starting teardown_class+++++++++++++')
+        cephdeploy.cleanupNodes(self.ctx['allnodes'], 
+                               'ceph')
+        log.info('++++++++++++++Completed teardown_class++++++++++++')
---
+++ utils/iscsi.py
@@ -0,0 +1,143 @@
+import logging
+import general
+from launch import launch
+
+
+
+log = logging.getLogger(__name__)
+
+
+def targetService(node, action):
+    status = {'start': 'Active: active', 'restart': 'Active: active', 'stop': 'Active: inactive'}
+    cmd = 'ssh %s sudo systemctl %s target' % (node, action)
+    stdout, strderr = general.eval_returns(cmd)
+    assert (status[action] in stdout.strip()), "error with target service on node %s" % node
+
+
+def addBlock(node, block_name, drive_name):
+    cmd = 'ssh %s sudo tcm_node --block %s %s' % (node, block_name, drive_name)
+    general.eval_returns(cmd)
+    cmd = 'ssh %s sudo tcm_node --listhbas %s' % (node, block_name)
+    stdout, strderr = general.eval_returns(cmd)
+    assert (drive_name in stdout.strip()), "error with iBlock creation on node %s" % node
+
+
+def addIQNTPG(node, iqn, tpg):
+    cmd = 'ssh %s sudo lio_node --listtargetnames'
+    stdout, strderr = general.eval_returns(cmd)
+    targets_list = stdout.split('\n')
+    targets_list = map(str.strip, targets_list)
+    if iqn in targets_list:
+        cmd = 'ssh %s sudo lio_node --listtpgparam %s %s' % (node, iqn, tpg)
+        rc, stdout, stderr = launch(cmd=cmd)
+        assert (rc != 0), "%s with %s already exist on node %s" % (iqn, tpg, node)
+    cmd = 'ssh %s sudo lio_node --addtpg %s %s' % (node, iqn, tpg)
+    general.eval_returns(cmd)
+    cmd = 'ssh %s sudo lio_node --listtargetnames'
+    stdout, strderr = general.eval_returns(cmd)
+    targets_list = stdout.split('\n')
+    targets_list = map(str.strip, targets_list)
+    assert (iqn in targets_list), "iqn %s was not created on node %s" % (iqn, node)
+    cmd = 'ssh %s sudo lio_node --listtpgparam %s %s' % (node, iqn, tpg)
+    rc, stdout, stderr = launch(cmd=cmd)
+    assert (rc == 0), "tpg %s was not created for iqn %s on node %s" % (tpg, iqn, node)
+    
+
+def addNP(node, iqn, tpg, ip, port):
+    cmd = 'ssh %s sudo lio_node --addnp %s %s %s:%s' % (node, iqn, tpg, ip, port)
+    general.eval_returns(cmd)
+    cmd = 'ssh %s sudo lio_node --listnps %s %s' % (node, iqn, tpg)
+    stdout, strderr = general.eval_returns(cmd)
+    ip_port = '%s:%s' % (ip, port)
+    assert (ip_port == stdout.strip()), "NP %s could not be added" % ip_port
+
+
+def disableAuth(node, iqn, tpg):
+    cmd = 'ssh %s sudo lio_node --disableauth %s %s' % (node, iqn, tpg)
+    general.eval_returns(cmd)
+
+
+def enableDemoMode(node, iqn, tpg):
+    cmd = 'ssh %s sudo lio_node --demomode %s %s' % (node, iqn, tpg)
+    general.eval_returns(cmd)
+
+
+def enableTPG(node, iqn, tpg):
+    cmd = 'ssh %s sudo lio_node --enabletpg %s %s' % (node, iqn, tpg)
+    general.eval_returns(cmd)
+
+
+def addLun(node, iqn, tpg, lun_num, lun_name, block_name):
+    cmd = 'ssh %s --addlun %s %s %s %s %s' % (node, iqn, tpg, lun_num, lun_name, block_name)
+    general.eval_returns(cmd)
+    lun_dir = '/sys/kernel/config/target/iscsi/%s/tpgt_%s/lun/lun_%s/%s' % (iqn, tpg, lun_num, lun_name)
+    cmd = 'ssh %s ls %s' % (node, lun_dir)
+    rc, stdout, stderr = launch(cmd=cmd)
+    assert (rc == 0), "lun %s was not added for block %s" % (lun_name, block_name)
+
+
+def demoModeWriteProtect(node, iqn, tpg):
+    file_path = '/sys/kernel/config/target/iscsi/%s/tpgt_%s/attrib/demo_mode_write_protect' % (iqn, tpg)
+    cmd = 'ssh %s sudo sh -c "echo 0 >> %s"' % (node, file_path)
+    general.eval_returns(cmd)
+    cmd = 'ssh %s cat %s' % (node, file_path)
+    stdout, strderr = general.eval_returns(cmd)
+    assert (str(stdout.strip()) == '0'), "demo mode write protect was not set on node %s" % node
+
+
+def discoverLoginTarget(client_node, target_node_ip, iqn, tpg, port):
+    cmd = 'ssh %s sudo iscsiadm -m discovery -p %s:%s -t sendtargets' % (client_node, target_node_ip, port)
+    stdout, strderr =  general.eval_returns(cmd)
+    assert(stdout.strip().split('\n') == 1), "more than one target found on %s for port %s" % (target_node_ip, port)
+    target = stdout.split('\n')[0].strip()
+    exp_target = '%s:%s,%s %s' % (target_node_ip, port, tpg, iqn)
+    assert (target == exp_target), "actual target %s did not match expected %s" % (target, exp_target)
+    
+    cmd = "ssh %s lsblk -io KNAME,TYPE,SIZE,MODEL | grep IBLOCK | awk '{print $1}'"
+    stdout, strderr =  general.eval_returns(cmd)
+    curr_iblocks = stdout.split('/n')
+   
+    cmd = 'ssh %s sudo iscsiadm -m node -T %s -p %s:%s --login' % (target_node_ip, iqn, target_node_ip, port)
+    stdout, strderr = general.eval_returns(cmd)
+    
+    validate_string = "Login to [iface: default, target: %s, portal: %s,%s] successful" % (iqn, target_node_ip, port)
+    assert (validate_string in stdout), "could not login to target with iqn %s" % iqn
+
+    cmd = "ssh %s lsblk -io KNAME,TYPE,SIZE,MODEL | grep IBLOCK | awk '{print $1}'"
+    stdout, strderr =  general.eval_returns(cmd)
+    new_iblocks = stdout.split('/n')
+
+    created_blocks = list(set(new_iblocks) - set(curr_iblocks))
+    assert (len(created_blocks) == 1), "length of target created was not one"
+   
+    return created_blocks[0].strip()
+
+
+def partitionIBlock(node, block):
+    cmd = "ssh %s lsblk -io KNAME,TYPE,SIZE,MODEL | awk '{print $1}' | grep %s" % (node, block)
+    stdout, strderr =  general.eval_returns(cmd)
+    drives = stdout.strip().split('\n')
+    assert (len(drives) == 1), "the block %s was either not found or already partitioned - %s" % (block, drives)
+
+    cmd = 'ssh %s echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/%s' % (node, block)
+    general.eval_returns(cmd)
+   
+    cmd = "ssh %s lsblk -io KNAME,TYPE,SIZE,MODEL | awk '{print $1}' | grep %s1" % (node, block)
+    stdout, strderr =  general.eval_returns(cmd)
+    drives = stdout.strip().split('\n')
+    assert (len(drives) == 1), "the block %s was not partitioned- %s" % (block, drives)
+  
+    return drives[0].strip()
+
+
+def createFSMount(node, drive, fs):
+    cmd = 'ssh %s sudo mkfs.%s /dev/%s' % (node, fs, drive)
+    general.eval_returns(cmd)
+    cmd = 'ssh %s rm -rf ~/iscsi_test' % node
+    general.eval_returns(cmd)
+    cmd = 'ssh %s mkdir ~/iscsi_test' % (node)
+    general.eval_returns(cmd) 
+    cmd = 'ssh %s sudo mount /dev/%s ~/iscsi_test' % (node, drive)
+    general.eval_returns(cmd)
+    cmd = 'ssh %s sudo chown jenkins:users ~/iscsi_test' % node
+    general.eval_returns(cmd)
