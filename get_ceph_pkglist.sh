ab899f739dab9ec48ea41808f9907b73e4960b0c
ksharma <ksharma@suse.com>
2015-07-02 16:02:40 +0200

    fixed typos

--- get_ceph_pkglist.sh
+++ get_ceph_pkglist.sh
@@ -2,19 +2,19 @@
 set +ex
 iosc='osc -A https://api.suse.de'
 
-if [ ! -d ~/workspace/Devel:Storage:2.0:Staging ] ; then
-   $iosc checkout Devel:Storage:2.0:Staging
+if [ ! -d ~/workspace/Devel:Storage:1.0:Staging ] ; then
+   $iosc checkout Devel:Storage:1.0:Staging
 fi
 
-#pkgs_list=$($iosc ls Devel:Storage:2.0:Staging | grep -v ^_)
+#pkgs_list=$($iosc ls Devel:Storage:1.0:Staging | grep -v ^_)
 pkgs_list="ceph ceph-deploy python-remoto python-execnet python-itsdangerous"
 
-pushd ~/workspace/Devel:Storage:2.0:Staging
+pushd ~/workspace/Devel:Storage:1.0:Staging
 $iosc up
 
 declare -a pkgs_arr=();
 for i in $pkgs_list; do
-        pkg_diff=`$iosc rdiff Devel:Storage:2.0:Staging $i Devel:Storage:2.0 $i | cat`
+        pkg_diff=`$iosc rdiff Devel:Storage:1.0:Staging $i Devel:Storage:1.0 $i | cat`
         if [ -n "$pkg_diff" ]; then
                 revision=`$iosc info $i | cat | grep Revision | awk '{print $2}'`
                 echo package $i:$revision will be copypacd
