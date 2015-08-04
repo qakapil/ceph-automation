7077e2d6d0e7ebf0e3dbfad4dc98494cc1905d2f
doliveira <doliveira@suse.com>
2015-08-04 08:52:14 -0600

    libiscsi-test.sh function name corrected 'runLibiSCSITests()'.

--- utils/libiscsi-test.sh
+++ utils/libiscsi-test.sh
@@ -12,7 +12,10 @@ runLibiSCSITests()
         ISCSI_PORT=$2
         IQN_ADDR=$3
 
-	#-- 
+	#-- For now, we are using LUN 0 by default, we will want to have it ...
+	#-- dynamic or read from the "iscsi-ls -s iscsi://10.160.223.10" command or ...
+	#-- from the python framework ... 
+	#-- "iscsi.addLun(iscsi_target['node'], iscsi_target['iqn'], iscsi_target['tpg'], '0', 'MiddlingThingPort', iscsi_target['block_name'])
 	#iscsi-test-cu -d -t ALL iscsi://10.160.223.10:3260/iqn.1996-04.de.suse:01:cd52c4d99b/
 	iscsi-test-cu -d -t ALL ${ISCSI_PROTOCOL}${IP_ADDR}:${ISCSI_PORT}/${IQN_ADDR}/0
 }
@@ -26,7 +29,7 @@ main()
 	LIBISCSI_UTIL_INSTALLED=$(which $LIBISCSI_NEEDED_UTIL 2>/dev/null)
 
 	if [ -x $LIBISCSI_UTIL_INSTALLED ]; then 
-		runLibISCSITests "${1}" "${2}" "${3}" 
+		runLibiSCSITests "${1}" "${2}" "${3}" 
 	else 
 		echo "Could not find: ${LIBISCSI_NEEDED_UTIL} " 
 	        echo "Aborting..." 
