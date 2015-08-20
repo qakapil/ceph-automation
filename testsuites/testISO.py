7308964fa5789bb1eaa15f8de3fec44e7beddcd6
ksharma <ksharma@suse.com>
2015-08-20 16:45:51 +0200

    debuging

--- testsuites/testISO.py
+++ testsuites/testISO.py
@@ -270,7 +270,7 @@ class TestSanity(basetest.Basetest):
     def test27_CreateRGW(self):
         for rgw in self.ctx['rgws']:
             apache = rgw.get('apache', None)
-            log.info('APACHE '+apache)
+            log.info('APACHE '+str(apache))
             rgw_tasks.create_rgw(rgw['rgw-host'], rgw['rgw-name'], rgw['rgw-port'], apache=apache)
         for rgw in self.ctx['rgws']:
             rgw_tasks.verifyRGWList(rgw['rgw-host'], rgw['rgw-name'])
