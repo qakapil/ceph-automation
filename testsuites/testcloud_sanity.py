6b3567dc2fcb2da5bbbebe4398ed33fb6ad2cc36
Vincent Untz <vuntz@suse.com>
2015-09-21 23:15:06 +0200

    testcloud: Update for SES2

--- testsuites/testcloud_sanity.py
+++ testsuites/testcloud_sanity.py
@@ -1,6 +1,7 @@
 from utils import basetest
 from utils import monitoring
 from utils import operations
+from utils import rbd_operations
 import logging,time,re, os
 #from nose import with_setup
 
@@ -31,7 +32,6 @@ class TestSanity(basetest.Basetest):
                               % (fsid,status)
         active_clean = False
         counter = 0
-        #default_pgs = str(self.ctx['default_pgs']).strip()
         default_pgs = monitoring.getTotalPGs()
         while not active_clean:
             if default_pgs +' active+clean' in status:
@@ -40,6 +40,7 @@ class TestSanity(basetest.Basetest):
                 active_clean = True
                 continue
             if (counter > 20):
+                os.environ["CLUSTER_FAILED"] = "Yes"
                 raise Exception, 'PGs did not reach active+clean state \
                                    after 5 mins'
             log.debug('waiting for 5 seconds for ceph status to update')
@@ -52,27 +53,61 @@ class TestSanity(basetest.Basetest):
         if 'health HEALTH_OK' in status:
             log.warning('cluster health is OK and PGs are active+clean')
 
-    def test02_ValidateCephVersion(self):
+    def test02_ValidateCephHealth(self):
+        fsid = monitoring.getFSID()
+        status = monitoring.getCephStatus()
+        if fsid not in status:
+            raise Exception, "fsid %s was not found in ceph status %s"\
+                              % (fsid,status)
+        health_ok = False
+        counter = 0
+        while not health_ok:
+            if (counter > 20):
+                raise Exception, 'cluster health was no OK'
+            log.debug('waiting for 5 seconds for ceph status to update')
+            time.sleep(120)
+            counter += 1
+            status = monitoring.getCephStatus()
+            if 'health HEALTH_WARN clock skew detected' in status:
+                log.warning('health HEALTH_WARN clock skew detected in\
+                             ceph status')
+            if 'health HEALTH_OK' in status:
+                health_ok = True
+                log.warning('cluster health is OK and PGs are active+clean')
+            else:
+                pass
+                #for node in self.ctx['initmons']:
+                    #operations.restartCeph(node)
+
+    def test03_ValidateCephVersion(self):
         expVersion = self.ctx['ceph_version']
         actVersion = monitoring.getActuaVersion()
         if expVersion not in actVersion:
             raise Exception, "expected '%s' and actual '%s' \
                 versions did not match" % (expVersion,actVersion)
 
-    def test03_ValidateDefaultPools(self):
+    def test04_ValidateDefaultPools(self):
         def_pools = monitoring.getDefaultPools()
-        assert ('0 data,1 metadata,2 rbd,' in def_pools),"The default \
+        assert ('0 rbd,' in def_pools),"The default \
         pools were %s" % def_pools
 
-    def test04_CreateImages(self):
-        for image in self.ctx['images']:
-            operations.createRBDImage(image)
+    def test05_CreatePools(self):
+        for pool in self.ctx['createpools']:
+            operations.createPool(pool)
 
-    def test05_RemoveImages(self):
-        for image in self.ctx['images']:
-            operations.rbdRemovePoolImage(image)
+    def test06_ValidatePools(self):
+        for pool in self.ctx['createpools']:
+            operations.validatePool(pool)
 
-    def test06_ValidateMonStat(self):
+    def test07_CreateImages(self):
+        for image in self.ctx['images']:
+            rbd_operations.createRBDImage(image)
+
+    def test08_RemoveImages(self):
+        for image in self.ctx['images']:
+            rbd_operations.rbdRemovePoolImage(image)
+
+    def test09_ValidateMonStat(self):
         mon_stat = monitoring.getMonStat()
         log.info("the mon stat is "+ str(mon_stat))
         matchObj = re.match( r'.*:(.*) mons at .* quorum (.*?) (.*)', mon_stat, re.M|re.I)
@@ -83,75 +118,37 @@ class TestSanity(basetest.Basetest):
         assert(sorted(self.ctx['initmons']) == sorted(matchObj.group(3).split(','))),\
         "the monlist in quorum was not as expected"
 
-    def test07_ValidateOSDStat(self):
+    def test10_ValidateOSDStat(self):
         osd_stat = monitoring.getOSDStat()
         n = len(self.ctx['osds'])
         expStr = "%s osds: %s up, %s in" % (n,n,n)
         assert(expStr in osd_stat),"osd stat validation failed"
 
-    def test08_RadosObjects(self):
+    def test11_RadosObjects(self):
         for radosobject in self.ctx['radosobjects']:
             operations.createValidateObject(radosobject)
         for radosobject in self.ctx['radosobjects']:
             operations.removeObject(radosobject)
 
-    def test09_CreatePools(self):
-        for pool in self.ctx['createpools']:
-            operations.createPool(pool)
-
-    def test10_ValidatePools(self):
-        for pool in self.ctx['createpools']:
-            operations.validatePool(pool)
-
-    def test11_DeletePools(self):
+    def test12_DeletePools(self):
         for pool in self.ctx['createpools']:
             operations.deletePool(pool)
 
-    def test12_Validatelibrbd(self):
+    def test13_Validatelibrbd(self):
         operations.validateLibRbdTests()
-        '''
-        from utils import librbd_tasks
-        for image in self.ctx['librbd_images']:
-            cluster = librbd_tasks.createCluster('/etc/ceph/ceph.conf')
-            log.info("created the cluster")
-            pool_ctx = librbd_tasks.createPoolIOctx(image['poolname'],cluster)
-            log.info("created the pool ctx")
-            librbd_tasks.createImage(image['size_gb'],image['imagename'],pool_ctx)
-            log.info("created the image")
-            imageslist = librbd_tasks.getImagesList(pool_ctx)
-            log.info("got the image list "+str(imageslist))
-            assert(image['imagename'] in imageslist),"image %s could \
-            was not created" %(image['imagename'])
-            image_ctx = librbd_tasks.createImgCtx(image['imagename'], pool_ctx)
-            log.info("created the image ctx")
-            size = librbd_tasks.getImageSize(image_ctx)
-            expsize = image['size_gb'] * 1024**3
-            size = str(int(size)).strip()
-            expsize = str(int(expsize)).strip()
-            log.info('actual image size is '+size)
-            log.info('expected image size is '+expsize)
-            assert(size == expsize),"image size not as expected"
-            stats = librbd_tasks.getImageStat(image_ctx)
-            log.info("the stats for the image "+image['imagename']+\
-            "are "+str(stats))
-            librbd_tasks.close_imgctx(image_ctx)
-            librbd_tasks.removeImage(pool_ctx, image['imagename'])
-            log.info("removed the image")
-            librbd_tasks.close_cluster(cluster, pool_ctx)
-        '''
 
-    def test13_ValidateDefaultOSDtree(self):
+    def test14_ValidateDefaultOSDtree(self):
         str_osd_tree = monitoring.getOSDtree()
         osd_tree = str_osd_tree.split('\n')
         for i in range(len(osd_tree)-1):
-            osd_tree[i] = osd_tree[i].split('\t')
-        indx = osd_tree[0].index('weight')
+            osd_tree[i] = osd_tree[i].split()
+        indx = osd_tree[0].index('WEIGHT')
         for i in range(len(osd_tree)-1):
             value = osd_tree[i][indx].strip()
             assert('0' != value),"the weight of the\
             osd was zero \n"+str_osd_tree
 
-    def test14_restartRadosGW(self):
+    def test15_restartRadosGW(self):
         operations.restartRadosGW(self.ctx['radosgw_node'])
 
     def tearDown(self):
